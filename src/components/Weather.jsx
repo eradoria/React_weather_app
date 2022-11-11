b72745acfb1f87b9c7580350fda0b4d1917713a6
eradoria <eradoria@gmail.com>
2022-11-11 17:55:35 -0600

    final

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -75,7 +75,7 @@ function Weather() {
 
   return (
     <div>
-      <div className="container mt-5">
+      <div className="container mt-5 ">
         <div className="row justify-content-center">
           <div className="col-md-4">
           <div class="card text-white text-center border-0">
